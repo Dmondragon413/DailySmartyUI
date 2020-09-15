f6758d183aba8ab4419238856bfa57bfa347690f
Dmondragon413 <mondragon0915@gmail.com>
2020-09-15 12:33:06 -0700

    modified fetchpostswithquery actionCreator to take in an optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,6 @@ export function fetchRecentPosts() {
     return function(dispatch) {
         axios.get('https://api.dailysmarty.com/posts')
         .then(response => {
-            console.log(response.data.posts);
             dispatch({
                 type: SET_RECENT_POSTS,
                 payload: response.data.posts
@@ -18,16 +17,16 @@ export function fetchRecentPosts() {
         })
     }
 }
-
-export function fetchPostsWithQuery(query) {
+// added perameter to the function below      vvv. a function being passed in from home.js
+export function fetchPostsWithQuery(query, callback) { // why is it called callback? what is it?
     return function(dispatch) {
         axios.get(`https://api.dailysmarty.com/search?q=${query}`)
         .then(response => {
-            console.log(response.data.posts)
             dispatch({
                 type: SET_RESULTS_POSTS,
                 payload: response.data.posts
             })
+            if (callback) { callback() } // call on function? and is this a short version of a ternary operator??
         })
     }
 }
