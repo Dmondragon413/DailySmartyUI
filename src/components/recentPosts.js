5ddb2191076df054e62a70949d9bfdb06761a717
Dmondragon413 <mondragon0915@gmail.com>
2020-09-15 22:14:46 -0700

    styled the recentPosts component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -27,7 +27,7 @@ class RecentPosts extends Component {
         return(
             <div className="recent-posts">
                 <div className="recent-posts-wrapper">
-                    <div className="recent-post-heading">Recent Posts</div>
+                    <div className="recent-posts-heading">Recent Posts</div>
                     <ul className="recent-posts-posts">
                         {this.renderPosts()}
                     </ul>
